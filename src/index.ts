61062961e3a704918aab4ae91fe902aac38f772c
nikita <nikita.a@halykmarket.com>
2025-01-21 19:05:40 +0500

    added new params for may be normal work project

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,12 @@ export default {
         console.log('modalMethods have been added to globalProperties');
     },
 };
+declare module '@vue/runtime-core' {
+    interface ComponentCustomProperties {
+        $modal: {
+            open: (name: string, options?: Record<string, any>) => void;
+            close: (id: number) => void;
+        };
+    }
+}
+
